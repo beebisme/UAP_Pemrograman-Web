871cc110994d508c20798b4c398c6ff217ad3302
beebisme <100251496+beebisme@users.noreply.github.com>
2023-12-28 11:50:29 +0700

    fix bug

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ productName = ["A", "B", "C"];
 productPrice = [10000, 20000, 30000];
 
 var message = "";
-var codeValue = productCodeForm.value;
-var qty = productQtyForm.value;
-var total = 0;
+var total;
 
 btnSubmit.addEventListener("click", () => {
+  var codeValue = productCodeForm.value;
+  var qty = productQtyForm.value;
   for (var i = 0; i < 3; i++) {
     if (productCode[i] == codeValue) {
       total = productPrice[i] * qty;
       message += `Total Pembayaran Adalah : ${total} \n`;
       alert(message);
+      var uang = prompt("masukkan uang anda");
+      uang = parseInt(uang);
+
+      if (uang > total) {
+        var kembalian = uang - total;
+        alert(`total kembalian anda adalah ${kembalian}`);
+      }
     }
   }
 });
-
-var uang = prompt("masukkan uang anda");
-
-if (uang > total) {
-  var kembalian = total - uang;
-  alert(`total kembalian anda adalah ${kembalian}`);
-}
